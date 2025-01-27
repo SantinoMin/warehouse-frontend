a2c0b3241a711fcb580b0f0f0fce55cb74f1150c
SantinoMin <santinomin0381@gmail.com>
2025-01-28 01:51:51 +0900

    fix : vite fixed.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 //rfce로 기본 세팅
 import MainPage from '@pages/index/index';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { RecoilRoot } from 'recoil'
 
 function App() {
   return (
-    <RecoilRoot>
     <BrowserRouter>
       <Routes>
         <Route index path="/" element={<MainPage />}></Route>
       </Routes>
     </BrowserRouter>
-    </RecoilRoot>
   );
 }
 
